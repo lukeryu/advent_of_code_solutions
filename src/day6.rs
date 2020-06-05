ccb4cb803906ea23dc16cf5d06447976e30c855c
lukeryu <lukeryu@gmail.com>
2020-06-05 18:58:27 -0500

    Solved day 6 puzzle

--- src/day6.rs
+++ src/day6.rs
@@ -2,75 +2,145 @@ use std::collections::HashMap;
 
 struct Orbit {
     orbited: String,
-    orbiting: String
+    orbiting: String,
 }
 
 impl Orbit {
     fn new(orbited: String, orbiting: String) -> Orbit {
         return Orbit {
-            orbited: orbited,
-            orbiting: orbiting
+            orbited,
+            orbiting,
         };
     }
 }
 
 struct CelestialBody {
     name: String,
-    parents: Vec<CelestialBody>,
-    children: Vec<CelestialBody>
+    parents: Vec<String>,
+    children: Vec<String>,
 }
 
 impl CelestialBody {
-    fn addChild(mut self, mut childBody: CelestialBody) {
+    fn addChild(&mut self, childBody: String) {
         self.children.push(childBody);
-        childBody.parents.push(self);
+    }
+
+    fn addParent(&mut self, parentBody: String) {
+        self.parents.push(parentBody);
     }
 
     fn new(name: String) -> CelestialBody {
         return CelestialBody {
-            name: name,
+            name,
             parents: Vec::new(),
-            children: Vec::new()
+            children: Vec::new(),
+        };
+    }
+
+    fn orbit_count(&self, map: &HashMap<String, CelestialBody>) -> u32 {
+        if self.parents.is_empty() {
+            return 0;
         }
+
+        let x: u32 = self.parents.iter()
+            .map(|name| -> u32 {
+                map.get(name).map_or(0, |body| body.orbit_count(map))
+            })
+            .sum();
+        return x + 1u32;
+    }
+
+    fn parents_and_children(&self) -> Vec<String> {
+        let mut vector = Vec::new();
+        vector.extend(self.parents.clone());
+        vector.extend(self.children.clone());
+
+        return vector;
+    }
+
+    fn find_santa(&self, map: &HashMap<String, CelestialBody>, visited: Vec<String>) -> u32 {
+        if visited.contains(&self.name) {
+            return 0;
+        }
+        if (self.name == String::from("SAN")){
+            return (visited.len() - 2) as u32;
+        }
+        for associate_name in self.parents_and_children().iter() {
+
+            let mut asdf: Vec<String> = Vec::new();
+            asdf.extend(visited.clone());
+            asdf.push(self.name.clone());
+
+            let result = match map.get(associate_name) {
+                Some(body) => body.find_santa(map, asdf),
+                None => 0
+            };
+
+            if (result > 0) {
+                return result;
+            }
+        }
+        return 0;
     }
 }
 
-fn getOrCreate(mut celestialBodyMap: HashMap<String, CelestialBody>, bodyName: String) -> CelestialBody {
-    let body = CelestialBody::new(bodyName);
-    return celestialBodyMap.entry(bodyName)
-        .get_or_insert(body);
+fn make_orbit_graph(input: Vec<String>) -> HashMap<String, CelestialBody> {
+    let orbits: Vec<Orbit> = input.iter()
+        .map(|inputString| -> Orbit {
+            let split: Vec<&str> = inputString.split(")")
+                .collect();
+            let orbited = String::from(split[0]);
+            let orbiting = String::from(split[1]);
+            return Orbit::new(
+                orbited,
+                orbiting,
+            );
+        }).collect();
+
+    let mut celestial_body_map: HashMap<String, CelestialBody> = HashMap::new();
+
+    for orbit in orbits.iter() {
+        let mut orbited_body = celestial_body_map.entry(orbit.orbited.clone())
+            .or_insert(CelestialBody::new(orbit.orbited.clone()));
+        orbited_body.addChild(orbit.orbiting.clone());
+    }
+
+    for orbit in orbits.iter() {
+        let mut orbiting_body = celestial_body_map.entry(orbit.orbiting.clone())
+            .or_insert(CelestialBody::new(orbit.orbiting.clone()));
+        orbiting_body.addParent(orbit.orbited.clone());
+    }
+
+    return celestial_body_map;
 }
 
-fn puzzle1(input: Vec<String>) -> i32 {
-//    let orbits = input.iter()
-//        .map(|s| -> Orbit {
-//            let split = s.split("(").collect();
-//            return Orbit::new(
-//                orbited: split[0],
-//                orbiting: split[1]
-//            );
-//        }).collect();
-//
-//    let mut celestialBodyMap: HashMap<String, CelestialBody> = HashMap::new();
+fn puzzle1(input: Vec<String>) -> u32 {
+    let celestial_body_map = make_orbit_graph(input);
 
-//    for orbit in orbits.iter() {
-//        let mut orbitedBody = getOrCreate(celestialBodyMap, orbit.orbited);
-//        let mut orbitingBody = getOrCreate(celestialBodyMap, orbit.orbiting);
-//        orbitedBody.addChild(orbitingBody);
-//    }
-
-    return 0;
+    return celestial_body_map.values()
+        .map(|body| body.orbit_count(&celestial_body_map))
+        .sum();
 }
 
-fn puzzle2(input: Vec<String>) -> i32 {}
+fn puzzle2(input: Vec<String>) -> u32 {
+    let celestial_body_map = make_orbit_graph(input);
+
+    let origin_body = celestial_body_map.get("YOU");
+
+    return match origin_body {
+        Some(body) => body.find_santa(&celestial_body_map, Vec::new()),
+        None => 0
+    }
+}
 
 #[cfg(test)]
 mod tests {
     use crate::utils;
     use crate::day6::{puzzle1, puzzle2};
+
     struct Puzzle1Test {
         test_data: Vec<String>,
-        expected_result: i32,
+        expected_result: u32,
     }
 
     #[test]
@@ -94,9 +164,10 @@ mod tests {
             Ok(lines) => {
                 tests.push(Puzzle1Test {
                     test_data: lines,
-                    expected_result: 0,
+                    expected_result: 322508,
                 });
                 for test in tests {
+                    println!("{}", "test");
                     let result = puzzle1(test.test_data);
                     assert_eq!(result, test.expected_result);
                 }
@@ -109,29 +180,34 @@ mod tests {
 
     struct Puzzle2Test {
         test_data: Vec<String>,
-        expected_result: i32,
+        expected_result: u32,
     }
 
     #[test]
     fn test_puzzle_2() {
         let mut tests: Vec<Puzzle2Test> = Vec::new();
         tests.push(Puzzle2Test {
-            test_data: vec![String::from("14")],
-            expected_result: 2,
+                test_data: vec![String::from("COM)B"),
+                                String::from("B)C"),
+                                String::from("C)D"),
+                                String::from("D)E"),
+                                String::from("E)F"),
+                                String::from("B)G"),
+                                String::from("G)H"),
+                                String::from("D)I"),
+                                String::from("E)J"),
+                                String::from("J)K"),
+                                String::from("K)L"),
+                                String::from("K)YOU"),
+                                String::from("I)SAN")],
+                expected_result: 4,
         });
-        tests.push(Puzzle2Test {
-            test_data: vec![String::from("1969")],
-            expected_result: 966,
-        });
-        tests.push(Puzzle2Test {
-            test_data: vec![String::from("100756")],
-            expected_result: 50346,
-        });
-        match utils::read_lines("data/Day1.txt") {
+
+        match utils::read_lines("data/Day6.txt") {
             Ok(lines) => {
                 tests.push(Puzzle2Test {
                     test_data: lines,
-                    expected_result: 4687331,
+                    expected_result: 496,
                 });
                 for test in tests {
                     let result = puzzle2(test.test_data);
