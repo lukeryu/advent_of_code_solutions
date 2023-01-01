9e8a4d6b1f8adb67e988218febcb8ede1897efb1
lukeryu <lukeryu@gmail.com>
2023-01-01 17:24:15 -0600

    solve day 15 of 2022

--- 2022_solutions/rust/src/day15.rs
+++ 2022_solutions/rust/src/day15.rs
@@ -1,6 +1,9 @@
+use std::cmp::{max, min};
+use std::collections::{HashSet, VecDeque};
+use std::ops::Range;
 use lazy_static::lazy_static;
 use regex::Regex;
-use crate::cartesian::{Grid, Point};
+use crate::cartesian::{Point};
 
 lazy_static! {
     static ref REGEX: Regex = Regex::new(r"^Sensor at x=(-?\d+), y=(-?\d+): closest beacon is at x=(-?\d+), y=(-?\d+)$").unwrap();
@@ -9,6 +12,17 @@ lazy_static! {
 struct Sensor {
     beacon: Point<i64>,
     sensor: Point<i64>,
+    radius: i64,
+}
+
+impl Sensor {
+    fn new(beacon: Point<i64>, sensor: Point<i64>) -> Self {
+        Self {
+            beacon,
+            sensor,
+            radius: sensor.distance_between(beacon)
+        }
+    }
 }
 
 fn parse_sensors(input_array: &[&str]) -> Vec<Sensor> {
@@ -26,10 +40,7 @@ fn parse_sensors(input_array: &[&str]) -> Vec<Sensor> {
         let beacon_point = Point::new(beacon_x, beacon_y);
 
 
-        let sensor = Sensor {
-            sensor: sensor_point,
-            beacon: beacon_point,
-        };
+        let sensor = Sensor::new(beacon_point, sensor_point);
 
         vec.push(sensor);
     }
@@ -37,37 +48,99 @@ fn parse_sensors(input_array: &[&str]) -> Vec<Sensor> {
     return vec;
 }
 
+fn solve(
+    beacons: &[Sensor],
+    target_y: i64,
+    max_x: Option<i64>,
+) -> (Vec<Range<i64>>, i64) {
+
+    let mut regions: Vec<Range<i64>> = vec![];
+    let mut extra: HashSet<Point<i64>> = HashSet::new();
+    for beacon in beacons.iter() {
+        let x = beacon.sensor.x;
+        let from_target =
+            beacon.radius - (beacon.sensor.y.abs_diff(target_y) as i64);
+        if from_target >= 0 {
+            let width = from_target;
+            let mut min = x - width;
+            let mut max = x + width + 1;
+            if let Some(max_x) = max_x {
+                min = 0.max(min.min(max_x));
+                max = (max_x).min(max);
+            }
+            let range = (min)..(max);
+            if beacon.sensor.y == target_y && range.contains(&beacon.sensor.x) {
+                extra.insert(beacon.sensor);
+            }
+            if beacon.sensor.y == target_y && range.contains(&beacon.beacon.x) {
+                extra.insert(beacon.beacon);
+            }
+            regions.push(range);
+        }
+    }
+    return (regions, extra.len() as i64)
+}
+
+fn reduce(mut regions: Vec<Range<i64>>) -> VecDeque<Range<i64>> {
+    let mut stack: VecDeque<Range<i64>> = VecDeque::new();
+    regions.sort_by(|a, b| a.start.cmp(&b.start));
+    let rng = regions.remove(0);
+    stack.push_front(rng);
+    for i in 0..regions.len() {
+        let Some(top) = stack.pop_front() else  {break;};
+        let Some(next) = regions.get(i) else {break;};
+
+        if top.end < next.start {
+            stack.push_back(top);
+            stack.push_front(next.clone());
+        } else if top.end < next.end {
+            stack.push_front((top.start)..(next.end));
+        } else {
+            stack.push_front(top);
+        }
+    }
+    return stack;
+}
+
 fn puzzle1(input_array: &[&str], at_row: i64) -> usize {
     let parsed_sensors = parse_sensors(input_array);
 
-    println!("sensors: {}", parsed_sensors.len());
+    let mut min_x = 0;
+    let mut max_x = 0;
 
-    let mut grid = Grid::<i64, bool>::new();
+    for sensor in parsed_sensors.iter() {
+        let xminus = sensor.sensor.x - sensor.radius;
+        let xplus = sensor.sensor.x + sensor.radius;
 
-    for sensor in parsed_sensors {
-        let distance = sensor.sensor.distance_between(sensor.beacon);
+        min_x = min(min_x, xminus);
+        max_x = max(max_x, xplus);
+    }
 
-        for x_delta in 0..distance {
-            for y_delta in 0..(distance - x_delta) {
-                let point1 = Point::new(sensor.sensor.x + x_delta, sensor.sensor.y + y_delta);
-                let point2 = Point::new(sensor.sensor.x + x_delta, sensor.sensor.y - y_delta);
-                let point3 = Point::new(sensor.sensor.x - x_delta, sensor.sensor.y + y_delta);
-                let point4 = Point::new(sensor.sensor.x - x_delta, sensor.sensor.y - y_delta);
+    let mut count = 0;
+    for column in min_x..=max_x {
+        let point = Point::new(column, at_row);
+        let can_be_sensed = parsed_sensors.iter()
+            .any(|sensor| sensor.sensor.distance_between(point) <= sensor.radius);
 
-                grid.set_value_at(point1, true);
-                grid.set_value_at(point2, true);
-                grid.set_value_at(point3, true);
-                grid.set_value_at(point4, true);
-            }
+        if can_be_sensed {
+            count = count + 1;
         }
     }
 
-    println!("len {}", grid.len());
-
-    return grid.matches_row_and_value(at_row, true);
+    return count - 1;
 }
 
-fn puzzle2(input_array: &[&str]) -> usize {
+fn puzzle2(input_array: &[&str], row_size: i64) -> i64 {
+    let parsed_sensors = parse_sensors(input_array);
+
+    for y in 0..row_size {
+        let (regions, _) = solve(&parsed_sensors, y, Some(row_size));
+        let stack = reduce(regions);
+        if stack.len() > 1 {
+            return ((stack[1].end) * 4000000) + y;
+        }
+    }
+
     return 0;
 }
 
@@ -93,41 +166,39 @@ mod tests {
         "Sensor at x=20, y=1: closest beacon is at x=15, y=3"];
 
     #[test]
-    #[ignore]
     fn test_puzzle1() {
         let return_value = puzzle1(&TEST_DATA, 10);
-        assert_eq!(return_value, 13);
+        assert_eq!(return_value, 26);
     }
 
     #[test]
     #[ignore]
     fn test_puzzle1_realdata() {
         let mut data = Vec::new();
-        let vec = read_file_strings("../data/Day13.txt");
+        let vec = read_file_strings("../data/Day15.txt");
         vec.iter()
             .for_each(|string| data.push(string.as_str().trim()));
 
         let return_value = puzzle1(&data, 2000000);
-        assert_eq!(return_value, 102399);
+        assert_eq!(return_value, 5878678);
     }
 
 
     #[test]
-    #[ignore]
     fn test_puzzle2() {
-        let return_value = puzzle2(&TEST_DATA);
-        assert_eq!(return_value, 2713310158);
+        let return_value = puzzle2(&TEST_DATA, 20);
+        assert_eq!(return_value, 56000011);
     }
 
     #[test]
     #[ignore]
     fn test_puzzle2_realdata() {
         let mut data = Vec::new();
-        let vec = read_file_strings("../data/Day13.txt");
+        let vec = read_file_strings("../data/Day15.txt");
         vec.iter()
             .for_each(|string| data.push(string.as_str().trim()));
 
-        let return_value = puzzle2(&data);
+        let return_value = puzzle2(&data, 4000000);
         assert_eq!(return_value, 23641658401);
     }
 }
