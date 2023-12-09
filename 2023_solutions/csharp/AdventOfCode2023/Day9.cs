c603b96bd493b9932938793bf160a9dad18e10f8
lukeryu <lukeryu@gmail.com>
2023-12-09 11:06:04 -0600

    Unified code from 2023 Day 9 Puzzles 1 and 2

--- 2023_solutions/csharp/AdventOfCode2023/Day9.cs
+++ 2023_solutions/csharp/AdventOfCode2023/Day9.cs
@@ -2,108 +2,66 @@
 
 public static class Day9
 {
+    private delegate T GetExtrapolateValue<T>(LinkedList<T> list);
+
+    private delegate void AddExtrapolateValue<T>(LinkedList<T> list, T valueToAdd);
+
+    private delegate T CalculateExtrapolateValue<T>(T lhs, T rhs);
+
     public static long Puzzle1(List<string> inputs)
     {
-        long sum = 0;
-        foreach (var input in inputs)
-        {
-            sum += Extrapolate(input);
-        }
-
-        return sum;
-    }
-
-    private static long Extrapolate(string input)
-    {
-        List<List<long>> rows = new();
-        var values = input.Split(' ').Select(long.Parse).ToList();
-        rows.Add(values);
-
-        bool allZeros = false;
-        while (!allZeros)
-        {
-            List<long> currentRow = new();
-
-            var previousRow = rows[^1];
-            for (int i = 1; i < previousRow.Count; i++)
-            {
-                currentRow.Add(previousRow[i] - previousRow[i - 1]);
-            }
-
-            rows.Add(currentRow);
-
-            if (currentRow.All((value) => value == 0))
-            {
-                allZeros = true;
-            }
-        }
-
-        rows[^1].Add( 0);
-
-        for (int i = rows.Count - 2; i >= 0; i--)
-        {
-            var currentRow = rows[i];
-            var previousRow = rows[i + 1];
-
-            var currentValue = currentRow[^1];
-            var previousValue = previousRow[^1];
-            
-            currentRow.Add(currentValue + previousValue);
-        }
-
-        return rows[0][^1];
+        GetExtrapolateValue<long> getValue = (list) => list.Last!.Value;
+        AddExtrapolateValue<long> addValue = (list, valueToAdd) => list.AddLast(valueToAdd);
+        CalculateExtrapolateValue<long> calcValue = (lhs, rhs) => lhs + rhs;
+        return inputs.Sum(input => Extrapolate(input, getValue, addValue, calcValue));
     }
 
     public static long Puzzle2(List<string> inputs)
     {
-        long sum = 0;
-        foreach (var input in inputs)
-        {
-            sum += ExtrapolateBackwards(input);
-        }
-
-        return sum;
+        GetExtrapolateValue<long> getValue = (list) => list.First!.Value;
+        AddExtrapolateValue<long> addValue = (list, value) => list.AddFirst(value);
+        CalculateExtrapolateValue<long> calcValue = (lhs, rhs) => lhs - rhs;
+        return inputs.Sum(input => Extrapolate(input, getValue, addValue, calcValue));
     }
-    
-    private static long ExtrapolateBackwards(string input)
+
+    private static long Extrapolate(string input, GetExtrapolateValue<long> getValue,
+        AddExtrapolateValue<long> addValue, CalculateExtrapolateValue<long> calculateExtrapolateValue)
     {
         List<LinkedList<long>> rows = new();
         var values = new LinkedList<long>(input.Split(' ').Select(long.Parse).ToList());
         rows.Add(values);
 
-        bool allZeros = false;
-        while (!allZeros)
+        while (true)
         {
             LinkedList<long> currentRow = new();
 
             var previousRow = rows[^1];
-            for (int i = 1; i < previousRow.Count; i++)
+            for (var elementIndex = 1; elementIndex < previousRow.Count; elementIndex++)
             {
-                long value = previousRow.ElementAt(i) - previousRow.ElementAt(i - 1);
-                currentRow.AddLast(value);
+                currentRow.AddLast(previousRow.ElementAt(elementIndex) - previousRow.ElementAt(elementIndex - 1));
             }
 
             rows.Add(currentRow);
 
             if (currentRow.All((value) => value == 0))
             {
-                allZeros = true;
+                break;
             }
         }
 
-        rows[^1].AddFirst( 0);
+        rows[^1].AddLast(0);
 
-        for (int i = rows.Count - 2; i >= 0; i--)
+        for (var rowIndex = rows.Count - 2; rowIndex >= 0; rowIndex--)
         {
-            var currentRow = rows[i];
-            var previousRow = rows[i + 1];
+            var currentRow = rows[rowIndex];
+            var previousRow = rows[rowIndex + 1];
 
-            var currentValue = currentRow.First.Value;
-            var previousValue = previousRow.First.Value;
-            
-            currentRow.AddFirst(currentValue - previousValue);
+            var currentValue = getValue(currentRow);
+            var previousValue = getValue(previousRow);
+
+            addValue(currentRow, calculateExtrapolateValue(currentValue, previousValue));
         }
 
-        return rows[0].First.Value;
+        return getValue(rows[0]);
     }
 }
