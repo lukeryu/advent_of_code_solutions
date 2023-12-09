db7e90889ffa93868ab71828fcf78de9f2f298bb
lukeryu <lukeryu@gmail.com>
2023-12-08 23:43:50 -0600

    Add solution for 2023 Day9

---
+++ 2023_solutions/csharp/AdventOfCode2023/Day9.cs
@@ -0,0 +1,109 @@
+﻿namespace AdventOfCode2023;
+
+public static class Day9
+{
+    public static long Puzzle1(List<string> inputs)
+    {
+        long sum = 0;
+        foreach (var input in inputs)
+        {
+            sum += Extrapolate(input);
+        }
+
+        return sum;
+    }
+
+    private static long Extrapolate(string input)
+    {
+        List<List<long>> rows = new();
+        var values = input.Split(' ').Select(long.Parse).ToList();
+        rows.Add(values);
+
+        bool allZeros = false;
+        while (!allZeros)
+        {
+            List<long> currentRow = new();
+
+            var previousRow = rows[^1];
+            for (int i = 1; i < previousRow.Count; i++)
+            {
+                currentRow.Add(previousRow[i] - previousRow[i - 1]);
+            }
+
+            rows.Add(currentRow);
+
+            if (currentRow.All((value) => value == 0))
+            {
+                allZeros = true;
+            }
+        }
+
+        rows[^1].Add( 0);
+
+        for (int i = rows.Count - 2; i >= 0; i--)
+        {
+            var currentRow = rows[i];
+            var previousRow = rows[i + 1];
+
+            var currentValue = currentRow[^1];
+            var previousValue = previousRow[^1];
+            
+            currentRow.Add(currentValue + previousValue);
+        }
+
+        return rows[0][^1];
+    }
+
+    public static long Puzzle2(List<string> inputs)
+    {
+        long sum = 0;
+        foreach (var input in inputs)
+        {
+            sum += ExtrapolateBackwards(input);
+        }
+
+        return sum;
+    }
+    
+    private static long ExtrapolateBackwards(string input)
+    {
+        List<LinkedList<long>> rows = new();
+        var values = new LinkedList<long>(input.Split(' ').Select(long.Parse).ToList());
+        rows.Add(values);
+
+        bool allZeros = false;
+        while (!allZeros)
+        {
+            LinkedList<long> currentRow = new();
+
+            var previousRow = rows[^1];
+            for (int i = 1; i < previousRow.Count; i++)
+            {
+                long value = previousRow.ElementAt(i) - previousRow.ElementAt(i - 1);
+                currentRow.AddLast(value);
+            }
+
+            rows.Add(currentRow);
+
+            if (currentRow.All((value) => value == 0))
+            {
+                allZeros = true;
+            }
+        }
+
+        rows[^1].AddFirst( 0);
+
+        for (int i = rows.Count - 2; i >= 0; i--)
+        {
+            var currentRow = rows[i];
+            var previousRow = rows[i + 1];
+
+            var currentValue = currentRow.First.Value;
+            var previousValue = previousRow.First.Value;
+            
+            currentRow.AddFirst(currentValue - previousValue);
+        }
+
+        return rows[0].First.Value;
+    }
+}
---
+++ 2023_solutions/csharp/AdventOfCode2023Tests/Day9Tests.cs
@@ -0,0 +1,46 @@
+﻿using AdventOfCode2023;
+
+namespace AdventOfCode2023Tests;
+
+public class Day9Tests
+{
+    private static List<string> GetTestData()
+    {
+        return new List<string>
+        {
+            "0 3 6 9 12 15",
+            "1 3 6 10 15 21",
+            "10 13 16 21 30 45"
+        };
+    }
+
+    [Test]
+    public void Puzzle1TestData()
+    {
+        var result = Day9.Puzzle1(GetTestData());
+        Assert.That(result, Is.EqualTo(114));
+    }
+
+    [Test]
+    public void Puzzle1RealData()
+    {
+        var data = TestUtils.GetDataFromFile("Day9.txt");
+        var result = Day9.Puzzle1(data);
+        Assert.That(result, Is.EqualTo(2075724761));
+    }
+
+    [Test]
+    public void Puzzle2TestData()
+    {
+        var result = Day9.Puzzle2(GetTestData());
+        Assert.That(result, Is.EqualTo(2));
+    }
+
+    [Test]
+    public void Puzzle2RealData()
+    {
+        var data = TestUtils.GetDataFromFile("Day9.txt");
+        var result = Day9.Puzzle2(data);
+        Assert.That(result, Is.EqualTo(1072));
+    }
+}
